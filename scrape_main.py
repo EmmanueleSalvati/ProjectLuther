252103835c8670b10f760350334624ac1e56afbf
Emmanuele <emmanuele.salvati@gmail.com>
2015-01-23 11:27:18 -0500

    Fix scraping of BoxOfficeMojo

---
+++ scrape_main.py
@@ -0,0 +1,59 @@
+"""This is the main file to run to scrape movies"""
+
+import sys
+sys.path.append('/Users/JerkFace/Metis/Projects/Luther/')
+
+import scrape_utils
+import urllib2
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import pickle as pkl
+
+
+def list_links(url, pklname):
+    """Input: a url string; a pickle file name string;
+    Output: a selenium driver with that link"""
+
+    if not url:
+        url = sys.argv[1]
+    firefox = scrape_utils.driver_with_url(url)
+    links_dict = scrape_utils.urls_dict(firefox)
+    with open(pklname, 'w') as pklfile:
+        pkl.dump(links_dict, pklfile)
+
+    return links_dict
+
+
+if __name__ == '__main__':
+
+    url = 'http://boxofficemojo.com/genres/chart/?id=politicaldoc.htm'
+
+    ARGS = sys.argv[1:]
+    if len(ARGS) > 0:
+        url = ARGS[0]
+
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page)
+
+    # Scrape the table and put it into a dictionary
+    nav_tabs = soup.find(class_='nav_tabs')
+    movie_table = scrape_utils.find_following_table(nav_tabs)
+    movie_dict = scrape_utils.table_to_dict(movie_table)
+
+    # Put dictionary into a Dataframe
+    movies = pd.DataFrame.from_dict(movie_dict, orient='index')
+    movies.columns = ['Rank', 'Studio', 'TotalGross', 'TotalTheaters',
+                      'OpenGross', 'OpenTheatres', 'RelDate']
+    movies.replace('n/a', np.nan, inplace=True)
+    movies = movies.dropna()
+    movies['RelDate'] = movies['RelDate'].astype(datetime)
+    movies['TotalGross'] = movies['TotalGross'].\
+        apply(scrape_utils.money_to_int)
+    movies['OpenGross'] = movies['OpenGross'].apply(scrape_utils.money_to_int)
+    movies['TotalTheaters'] = movies['TotalTheaters'].\
+        apply(scrape_utils.remove_comma)
+    movies['OpenTheatres'] = movies['OpenTheatres'].\
+        apply(scrape_utils.remove_comma)
+    print movies.head()
--- scrape_utils.py
+++ scrape_utils.py
@@ -1,38 +1,31 @@
 """This module contains utility functions to scrape web data"""
 
-import urllib2
 from bs4 import BeautifulSoup
-import re
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# import re
 
 
-def table_from_soup(soup_obj):
-    """Given a BeautifulSoup object in BoxOfficeMojo, returns the movies'
-    table... WRONG!!"""
+# def table_to_links(movie_table):
+#     """Creates and returns a dictionary (movie title): (movie link);
 
-    nav_tabs = soup_obj.find(class_="nav_tabs")
-    movie_table = (nav_tabs.findNextSibling().findNextSibling().
-                   findNextSibling().findNextSibling())
+#     It takes a soup table as input, it retrieves all the links and
+#     finally creates the dictionary"""
 
-    return movie_table
+#     movies = {}
+#     links_from_table = movie_table.find_all('a')
+#     for links in links_from_table:
+#         link = links.get('href')
+#         movie = re.match('/movies/', link)
+#         if movie:
+#             movies[links.text] = 'boxofficemojo.com' + link
 
-
-def movie_links_from_table(movie_table):
-    """Creates and returns a dictionary (movie title): (movie link);
-
-    It takes a soup table as input, it retrieves all the links and
-    finally creates the dictionary"""
-
-    movies = {}
-    links_from_table = movie_table.find_all('a')
-    for links in links_from_table:
-        link = links.get('href')
-        movie = re.match('/movies/', link)
-        if movie:
-            movies[links.text] = 'boxofficemojo.com' + link
-
-    return movies
+#     return movies
 
 
+# SCRAPING UTILITIES
 def find_following_table(soup_tag):
     """Given an object of type <class 'bs4.element.Tag'>,
     find the first table after it"""
@@ -46,21 +39,80 @@ def find_following_table(soup_tag):
     return next_tag
 
 
-class movie(object):
-    """This class contains the following attributes:
-    title, lifetime gross, lifetime theaters, release date, perhaps more"""
+def table_to_dict(soup_table_tag):
+    """Input: a table of type <class 'bs4.element.Tag'>,
+    Output: a dict {title: [features]}"""
 
-    def __init__(self, title=None, life_gross=None, life_theaters=None,
-                 rank=None, studio=None, opening_gross=None,
-                 opening_theaters=None, rel_date=None):
-        self.title = title
-        self.life_gross = life_gross
-        self.life_theaters = life_theaters
-        self.rank = rank
-        self.studio = studio
-        self.opening_gross = opening_gross
-        self.opening_theaters = opening_theaters
-        self.rel_date = rel_date
+    movie_dict = {}
+    for row in soup_table_tag.findAll('tr'):
+        cells = row.findAll('td')
+        if len(cells) == 8:
+            rank = str(cells[0].find(text=True))
+            title = str(cells[1].find(text=True))
+            studio = str(cells[2].find(text=True))
+            tot_gross = str(cells[3].find(text=True))
+            tot_theatres = str(cells[4].find(text=True))
+            opn_gross = str(cells[5].find(text=True))
+            opn_theatres = str(cells[6].find(text=True))
+            rel_date = str(cells[7].find(text=True))
+            movie_dict[title] = [rank, studio, tot_gross, tot_theatres,
+                                 opn_gross, opn_theatres, rel_date]
+    return movie_dict
 
 
+def driver_with_url(politics_url):
+    """Input: http://boxofficemojo.com/genres/chart/?id=politicaldoc.htm
+    Output: urls of lists of political documentary"""
 
+    firefox = webdriver.Firefox()
+    firefox.get(politics_url)
+
+    return firefox
+
+
+def urls_dict(firefox):
+    """Input: a driver with url;
+    Output: a dictionary (page_range): (corresponding url)"""
+
+    print 'Creating dict...'
+    links_dict = {}
+    page_selector = ('//a[contains(@href, '
+                     '"/genres/chart/?view=main&sort=gross")]')
+    links_dict['first'] = str(firefox.current_url)
+
+    for page_anchor in firefox.find_elements_by_xpath(page_selector):
+        movies_range = str(page_anchor.text)
+        href = str(page_anchor.get_attribute('href'))
+        links_dict[movies_range] = 'http://boxofficemojo.com' + href
+
+    return links_dict
+
+
+# DATAFRAME MANIPULATION UTILITIES
+def money_to_int(moneystring):
+    """Input: a string like '$33,449,086';
+    Output: 33449086"""
+
+    return int(moneystring.replace(',', '').replace('$', ''))
+
+
+def remove_comma(number):
+    """Input: 1,100; Output: 1100"""
+
+    return int(number.replace(",", ''))
+
+# class movie(object):
+#     """This class contains the following attributes:
+#     title, lifetime gross, lifetime theaters, release date, perhaps more"""
+
+#     def __init__(self, title=None, life_gross=None, life_theaters=None,
+#                  rank=None, studio=None, opening_gross=None,
+#                  opening_theaters=None, rel_date=None):
+#         self.title = title
+#         self.life_gross = life_gross
+#         self.life_theaters = life_theaters
+#         self.rank = rank
+#         self.studio = studio
+#         self.opening_gross = opening_gross
+#         self.opening_theaters = opening_theaters
+#         self.rel_date = rel_date
