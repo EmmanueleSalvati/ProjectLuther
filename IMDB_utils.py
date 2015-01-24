24b0bca6d484f9123d9d194521a64823d4f5f8bd
Emmanuele <emmanuele.salvati@gmail.com>
2015-01-23 23:04:42 -0500

    get list of directors in IMDB

--- IMDB_utils.py
+++ IMDB_utils.py
@@ -53,7 +53,7 @@ def get_link(cell_soap):
     return link.get('href')
 
 
-# NEED TO FIX THIS ONE
+# CAN IMPROVE THIS FUNCTION
 def get_user_rating(cell_soap):
     """Input: a cell soap from IMDB; output: movie user rating"""
 
@@ -76,10 +76,72 @@ def get_num_voters(cell_soap):
     return full_string[4].replace('(', '').replace(',', '')
 
 
-def get_directors(cell_soap):
-    """Input: a cell soap from IMDB; output: movie list of directors"""
+# def uDir_to_Dir(element_u):
+#     """Input: 'u\n    Dir: ', output: 'Dir' WTF"""
+
+#     element_s = str(element_u)
+#     element_s = element_s.replace(" ", "")
+#     element_s = element_s.replace(":", "")
+#     element_s = element_s.replace("\n", "")
+
+#     return element_s
 
 
+def name_and_link(list_of_names):
+    """Input: [<a href="/name/nm0688636/">Laura Poitras</a>]
+    Output: ('Laura Poitras', "/name/nm0688636/")"""
+
+    names = []
+    links = []
+    for elem in list_of_names:
+        names.append(elem.text)
+        links.append(elem.get('href'))
+
+    return (names, links)
+
+
+def remove_commas_from_list(list_of_names):
+    """Remove commas from a list and returns the list"""
+
+    index_list = []
+    print list_of_names
+    for i in range(len(list_of_names)):
+        if i % 2 != 0:
+            index_list.append(i)
+
+    index_list.reverse()
+    for index in index_list:
+        list_of_names.pop(index)
+
+    return list_of_names
+
+
+def get_directors_and_actors_list(cell_soap):
+    """Input: a cell soap movie from IMDB;
+    output: movie list of directors and actors"""
+
+    directors = []
+    actors = []
+
+    children_list = []  # tmp list
+    movie_credits = cell_soap.find('span', attrs={'class': 'credit'})
+    for credit in movie_credits.children:
+        children_list.append(credit)
+
+    dir_num = 0
+    with_num = 0
+    for credit in children_list:
+        if 'Dir' in credit:
+            dir_num = children_list.index(credit)
+        if 'With' in credit:
+            with_num = children_list.index(credit)
+
+    directors.extend(children_list[dir_num + 1: with_num])
+    actors.extend(children_list[with_num + 1:])
+    # directors = remove_commas_from_list(directors)
+    # actors = remove_commas_from_list(actors)
+
+    return (directors, actors)
 
 
 def list_to_dict(movies_list):
